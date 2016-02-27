8967363c25bf7c2573e5cd9db24eb4c46c8e8cfe
Torey Hickman <torey@toreyhickman.com>
2016-02-26 16:12:49 -0800

    Return a cell index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,6 +7,6 @@ get '/colors/random' do
 
   #Create and return a JSON object with the random cell and color given below.
 
-  cell_to_update = rand(1..9)
+  cell_to_update = rand(9)
   color_hexcode  = dbc_color_hexcode # See helpers
 end
